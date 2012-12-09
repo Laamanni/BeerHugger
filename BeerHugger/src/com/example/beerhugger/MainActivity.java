9eab7560b37302d664fd9f548ebd51e52a71b2c9
Laamanni <laamanni@gmail.com>
2012-12-09 05:35:30 +0200

    pieniä muutoksia

--- BeerHugger/src/com/example/beerhugger/MainActivity.java
+++ BeerHugger/src/com/example/beerhugger/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends Activity {
   	// Reads an InputStream and converts it to a String.
   	 public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
   	     Reader reader = null;
-  	     reader = new InputStreamReader(stream, "ISO-8859-2");        
+  	     reader = new InputStreamReader(stream, "UTF-8");        
   	     char[] buffer = new char[len];
   	     reader.read(buffer);
   	     return new String(buffer);
