90760b2c7872fe07df0ecc469f62f37b248eb679
Laamanni <laamanni@gmail.com>
2012-12-18 07:23:29 +0200

    Lisätty gps-paikannus ja sen käyttö jos hakukenttä jätetään tyhjäksi.

--- BeerHugger/src/com/example/beerhugger/MainActivity.java
+++ BeerHugger/src/com/example/beerhugger/MainActivity.java
@@ -13,6 +13,9 @@ import java.net.URL;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -21,6 +24,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -31,19 +35,35 @@ public class MainActivity extends Activity {
     private TextView textView;
     private String baseUrl = "http://m.olutnorsu.fi/data.php?haku=";
     
+    private String location = "60.16985, 24.93855";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);   
+        setContentView(R.layout.activity_main);
         urlText = (EditText) findViewById(R.id.myUrl);
         textView = (TextView) findViewById(R.id.myText);
+        
+        // GPS
+        LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		LocationListener mlocListener = new MyLocationListener();
+		mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+		
     }
 
     // When user clicks button, calls AsyncTask.
     // Before attempting to fetch the URL, makes sure that there is a network connection.
     public void myClickHandler(View view) {
+
+    	// Inform user that somethings happening
+    	textView.setText("Retrieving data..");
+    	
         // Gets the URL from the UI's text field.
     	String stringUrl = urlText.getText().toString();
+    	
+    	if ( stringUrl.length() == 0 ) {
+    		stringUrl = location;
+    	}
     	
     	URI uri = null;
     	URL url = null;
@@ -137,5 +157,43 @@ public class MainActivity extends Activity {
   	     reader.read(buffer);
   	     return new String(buffer);
   	 }
+  	 
+  	/* Class My Location Listener */
+
+ 	public class MyLocationListener implements LocationListener
+ 	{
+ 		public void onLocationChanged(Location loc)
+ 		{
+ 			//loc.getLatitude();
+ 			//loc.getLongitude();
+ 			//String Text = "My current location is: " + "Latitude = " + loc.getLatitude() + "Longitude = " + loc.getLongitude();
+
+ 			location = loc.getLatitude() + ", " + loc.getLongitude();
+ 			
+ 			// Toast.makeText( getApplicationContext(),
+ 			//		Text,
+ 			//		Toast.LENGTH_SHORT).show();
+ 		}
+
+ 		public void onProviderDisabled(String provider)
+ 		{
+ 			textView.setText("Gps Disabled");
+ 			Toast.makeText( getApplicationContext(), "Gps Disabled", Toast.LENGTH_SHORT ).show();
+ 		}
+
+ 		public void onProviderEnabled(String provider)
+ 		{
+ 			textView.setText("Gps Enabled");
+ 			Toast.makeText( getApplicationContext(), "Gps Enabled",	Toast.LENGTH_SHORT).show();
+ 			
+ 		}
+
+
+ 		public void onStatusChanged(String provider, int status, Bundle extras)
+ 		{
+ 			
+ 		}
+
+ 	}/* End of Class MyLocationListener */
     
 }
